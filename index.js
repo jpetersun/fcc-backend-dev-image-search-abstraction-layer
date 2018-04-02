72d28c5d57075603fe64186814c9897facaf6318
James Peterson <jpeterson903@yahoo.com>
2018-04-02 10:26:37 -0700

    feat: set searchResult obj

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('/api/imagesearch/:searchTerm:offset', (req, res) => {
-  res.send(req.params)
+app.get('/api/imagesearch/:searchTerm', (req, res) => {
+  let url = null
+  let snippet = null
+  let thumbnail = null
+  let context = null
+
+  const searchResult = {
+    url,
+    snippet,
+    thumbnail,
+    context
+  }
+
+  res.send({
+    offset: req.query.offset,
+    searchTerm: req.params.searchTerm
+  })
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
