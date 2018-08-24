bcb663b0c609301ef7c2268237d8ccc72d8e0950
James Peterson <jpeterson903@yahoo.com>
2018-08-24 14:24:59 -0700

    style: use try/catch for async tests

--- test/test.js
+++ test/test.js
@@ -20,15 +20,13 @@ describe('Image Search API', () => {
   describe('GET /api/imagesearch', () => {
     // no need to use `done` with async
     it('should respond with 3 search terms', async () => {
-      await request(app)
-        .get('/api/imagesearch')
-        .then(res => {
-          res.type.should.equal('application/json')
-          res.body.should.have.lengthOf(3)
-        })
-        .catch(err => {
-          throw new Error(err)
-        })
+      try {
+        const res = await request(app).get('/api/imagesearch')
+        res.type.should.equal('application/json')
+        res.body.should.have.lengthOf(3)
+      } catch (err) {
+        throw new Error(err)
+      }
     })
 
     it('should respond with 10 search terms', async () => {
@@ -37,59 +35,51 @@ describe('Image Search API', () => {
       await seedDb(terms)
       await seedDb(terms)
 
-      await request(app)
-        .get('/api/imagesearch')
-        .then(res => {
-          res.type.should.equal('application/json')
-          res.body.should.have.lengthOf(10)
-        })
-        .catch(err => {
-          throw new Error(err)
-        })
+      try {
+        const res = await request(app).get('/api/imagesearch')
+        res.type.should.equal('application/json')
+        res.body.should.have.lengthOf(10)
+      } catch (err) {
+        throw new Error(err)
+      }
     })
   })
 
   describe('GET /api/imagesearch/:searchTerm', () => {
     it('should respond with search results', async () => {
-      await request(app)
-        .get('/api/imagesearch/galaxy')
-        .then(res => {
-          res.type.should.equal('application/json')
-          res.body.should.have.lengthOf(20)
-          res.body[0].should.have.property('imageURL')
-          res.body[0].should.have.property('tags')
-          res.body[0].should.have.property('previewURL')
-          res.body[0].should.have.property('pageURL')
-        })
-        .catch(err => {
-          throw new Error(err)
-        })
+      try {
+        const res = await request(app).get('/api/imagesearch/galaxy')
+        res.type.should.equal('application/json')
+        res.body.should.have.lengthOf(20)
+        res.body[0].should.have.property('imageURL')
+        res.body[0].should.have.property('tags')
+        res.body[0].should.have.property('previewURL')
+        res.body[0].should.have.property('pageURL')
+      } catch (err) {
+        throw new Error(err)
+      }
     })
 
     it('should respond with no search results', async () => {
-      await request(app)
-        .get('/api/imagesearch/_%20_')
-        .then(res => {
-          res.type.should.equal('application/json')
-          res.body.should.have.lengthOf(0)
-        })
-        .catch(err => {
-          throw new Error(err)
-        })
+      try {
+        const res = await request(app).get('/api/imagesearch/_%20_')
+        res.type.should.equal('application/json')
+        res.body.should.have.lengthOf(0)
+      } catch (err) {
+        throw new Error(err)
+      }
     })
   })
 
   describe('GET /api/imagesearch/:searchTerm?offset=Number', () => {
     it('should respond with offset search results', async () => {
-      await request(app)
-        .get('/api/imagesearch/sun?offset=5')
-        .then(res => {
-          res.type.should.equal('application/json')
-          res.body.should.have.lengthOf(5)
-        })
-        .catch(err => {
-          throw new Error(err)
-        })
+      try {
+        const res = await request(app).get('/api/imagesearch/sun?offset=5')
+        res.type.should.equal('application/json')
+        res.body.should.have.lengthOf(5)
+      } catch (err) {
+        throw new Error(err)
+      }
     })
   })
 })
